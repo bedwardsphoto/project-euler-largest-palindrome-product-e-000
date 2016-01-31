1bf93bb82e84e43d3b06e078d7baed6503aab261
bedwardsphoto <brian@brianedwardsphotography.com>
2016-01-31 12:25:22 -0500

    Done.

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,13 @@
-# Implement your procedural solution here!
+
+def largest_palindrome_product
+pal=Array.new
+999.downto(100) do |a|
+  999.downto(100) do |b|
+    total=a*b
+    if total.to_s==total.to_s.reverse
+      pal<<total
+    end
+  end
+end
+ return pal.max
+end
--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,1 +1,13 @@
-# Implement your object-oriented solution here!
+class LargestPalindromeProduct
+def answer
+  max=0
+999.downto(100) do |a|
+  a.downto(100) do |b|
+    total=a*b
+    max=total if total>max && total.to_s==total.to_s.reverse
+      break if total<=max
+    end
+  end
+  return max
+end
+end
